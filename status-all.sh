a808a6c29b2b3a69d90112e1416d54a0b079cd94
Dimitrios Kanoulas <dkanoulas@gmail.com>
2020-06-25 13:12:07 +0100

    Working version

--- status-all.sh
+++ status-all.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
 
-actual_dir=`pwd`
+rpl_dir=${PWD%/*}
+#echo "$rpl_dir"
+
 # if the script is sourced add the trap
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] && trap  "{ cd $actual_dir; trap - INT; return 1; }" INT
 
-robots_dir="$ROBOTOLOGY_ROOT/robots"
-external_dir="$ROBOTOLOGY_ROOT/external"
-configs_dir="$ROBOTOLOGY_ROOT/configs"
-
-#robots dir for each
-for f in `cd $robots_dir && ls -d -- */`
+#dir for each
+for f in `cd $rpl_dir && ls -d -- */`
 do
-    cd "$robots_dir/$f"
+    cd "$rpl_dir/$f"
+    echo "Current dir: " "$rpl_dir/$f"
     echo -e "\e[32m$f \e[39m --> `git status` \n"
     cd ..
 done
 
-#external dir for each
-for f in `cd $external_dir && ls -d -- */`
-do  
-    cd "$external_dir/$f"
-    echo -e "\e[31m$f \e[39m --> `git status` \n"
-    cd ..
-done
-
-#configs dir for each
-for f in `cd $configs_dir && ls -d -- */`
-do  
-    cd "$configs_dir/$f"
-    echo -e "\e[31m$f \e[39m --> `git status` \n"
-    cd ..
-done
-
-
 # come back to the dir where the script was executed
-cd $actual_dir
+cd $rpl_dir
 # possibly remove trap
 trap - INT
