6e4d3ebe867aaba31ae246acf9a817941f129da0
RPL AS UCL <60218079+rpl-as-ucl@users.noreply.github.com>
2020-10-25 10:31:26 +0000

    Update basic_setup.sh

--- basic_setup.sh
+++ basic_setup.sh
@@ -7,13 +7,13 @@ sudo apt-get install git cmake cmake-curses-gui build-essential gitg git-gui mel
 
 # ROS list and key
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 # Gazebo list and key
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 
-sudo apt-get update
+sudo apt update
 
 if [ "`lsb_release -cs`" = 'xenial' ]; then
         sudo apt-get install ros-kinetic-desktop-full
