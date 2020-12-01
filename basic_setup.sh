340e8a1a86861c0da44c6be49b54d814f91df1a2
RPL AS UCL <60218079+rpl-as-ucl@users.noreply.github.com>
2020-12-01 19:14:11 +0000

    Update basic_setup.sh

--- basic_setup.sh
+++ basic_setup.sh
@@ -24,31 +24,37 @@ sudo apt update
 # Ubuntu 16.04LTS
 if [ "`lsb_release -cs`" = 'xenial' ]; then
         sudo apt-get install ros-kinetic-desktop-full
-	sudo apt-get install ros-kinetic-eigen-conversions ros-kinetic-kdl-parser ros-kinetic-effort-controllers ros-kinetic-controller-manager ros-kinetic-transmission-interface
-        sudo apt-get install ros-kinetic-gazebo-ros-pkgs
+	sudo apt-get install ros-kinetic-eigen-conversions ros-kinetic-kdl-parser ros-kinetic-effort-controllers ros-kinetic-transmission-interface
+        sudo apt-get install ros-kinetic-gazebo-ros-*
+        sudo apt-get install ros-kinetic-joint-state* ros-joint-state-controller
 	sudo apt-get install ros-kinetic-combined-robot-hw
-        sudo apt-get install ros-joint-state-controller ros-kinetic-velocity-controllers ros-kinetic-twist-mux ros-kinetic-diff-drive-controller ros-kinetic-costmap-prohibition-layer ros-kinetic-moveit ros-kinetic-moveit-core ros-kinetic-teb-local-planner ros-kinetic-move-base ros-kinetic-moveit-kinematics ros-kinetic-robot-localization ros-kinetic-combined-robot-hw ros-kinetic-joint-limits-interface ros-kinetic-gmapping ros-kinetic-amcl ros-kinetic-position-controllers ros-kinetic-joint-trajectory-controller ros-kinetic-moveit-visual-tools ros-kinetic-moveit-ros-planning-interface ros-kinetic-ros-control ros-kinetic-ros-controllers ros-kinetic-global-planner ros-kinetic-joint-state-publisher-gui ros-kinetic-gazebo-ros-control
+        sudo apt-get install ros-kinetic-control-* ros-kinetic-controller-* ros-kinetic-effort-controllers ros-kinetic-position-controllers
+        sudo apt-get install ros-kinetic-velocity-controllers ros-kinetic-twist-mux ros-kinetic-diff-drive-controller ros-kinetic-costmap-prohibition-layer ros-kinetic-moveit ros-kinetic-moveit-core ros-kinetic-teb-local-planner ros-kinetic-move-base ros-kinetic-moveit-kinematics ros-kinetic-robot-localization ros-kinetic-combined-robot-hw ros-kinetic-joint-limits-interface ros-kinetic-gmapping ros-kinetic-amcl ros-kinetic-position-controllers ros-kinetic-joint-trajectory-controller ros-kinetic-moveit-visual-tools ros-kinetic-moveit-ros-planning-interface ros-kinetic-ros-control ros-kinetic-ros-controllers ros-kinetic-global-planner ros-kinetic-gazebo-ros-control
         sudo apt-get install ros-kinetic-catkin python-catkin-tools
 fi
 
 # Ubuntu 18.04LTS
 if [ "`lsb_release -cs`" = 'bionic' ]; then
         sudo apt-get install ros-melodic-desktop-full
-	sudo apt-get install ros-melodic-eigen-conversions ros-melodic-kdl-parser ros-melodic-effort-controllers ros-melodic-controller-manager ros-melodic-transmission-interface
-        sudo apt-get install ros-melodic-gazebo-ros-pkgs
+	sudo apt-get install ros-melodic-eigen-conversions ros-melodic-kdl-parser ros-melodic-effort-controllers ros-melodic-transmission-interface
+        sudo apt-get install ros-melodic-gazebo-ros-*
+	sudo apt-get install ros-melodic-joint-state* ros-joint-state-controller
 	sudo apt-get install ros-melodic-combined-robot-hw
-        sudo apt-get install ros-joint-state-controller ros-melodic-velocity-controllers ros-melodic-twist-mux ros-melodic-diff-drive-controller ros-melodic-costmap-prohibition-layer ros-melodic-moveit ros-melodic-moveit-core ros-melodic-teb-local-planner ros-melodic-move-base ros-melodic-moveit-kinematics ros-melodic-robot-localization ros-melodic-combined-robot-hw ros-melodic-joint-limits-interface ros-melodic-gmapping ros-melodic-amcl ros-melodic-position-controllers ros-melodic-joint-trajectory-controller ros-melodic-moveit-visual-tools ros-melodic-moveit-ros-planning-interface ros-melodic-ros-control ros-melodic-ros-controllers ros-melodic-global-planner ros-melodic-joint-state-publisher-gui ros-melodic-gazebo-ros-control
-        sudo apt-get install ros-melodic-catkin python-catkin-tools
+	sudo apt-get install ros-melodic-control-* ros-melodic-controller-* ros-melodic-effort-controllers ros-melodic-position-controllers	
+        sudo apt-get install ros-melodic-velocity-controllers ros-melodic-twist-mux ros-melodic-diff-drive-controller ros-melodic-costmap-prohibition-layer ros-melodic-moveit ros-melodic-moveit-core ros-melodic-teb-local-planner ros-melodic-move-base ros-melodic-moveit-kinematics ros-melodic-robot-localization ros-melodic-combined-robot-hw ros-melodic-joint-limits-interface ros-melodic-gmapping ros-melodic-amcl ros-melodic-position-controllers ros-melodic-joint-trajectory-controller ros-melodic-moveit-visual-tools ros-melodic-moveit-ros-planning-interface ros-melodic-ros-control ros-melodic-ros-controllers ros-melodic-global-planner ros-melodic-gazebo-ros-control
+        sudo apt-get install ros-melodic-catkin
 fi
 
 # Ubuntu 20.04LTS
 if [ "`lsb_release -cs`" = 'focal' ]; then
         sudo apt-get install ros-noetic-desktop-full
-	sudo apt-get install ros-noetic-eigen-conversions ros-noetic-kdl-parser ros-noetic-effort-controllers ros-noetic-controller-manager ros-noetic-transmission-interface
-        sudo apt-get install ros-noetic-gazebo-ros-pkgs
+	sudo apt-get install ros-noetic-eigen-conversions ros-noetic-kdl-parser ros-noetic-effort-controllers ros-noetic-transmission-interface
+        sudo apt-get install ros-noetic-gazebo-ros-*
+	sudo apt-get install ros-noetic-joint-state* ros-joint-state-controller
 	sudo apt-get install ros-noetic-combined-robot-hw
-        sudo apt-get install ros-joint-state-controller ros-noetic-velocity-controllers ros-noetic-twist-mux ros-noetic-diff-drive-controller ros-noetic-costmap-prohibition-layer ros-noetic-moveit ros-noetic-moveit-core ros-noetic-teb-local-planner ros-noetic-move-base ros-noetic-moveit-kinematics ros-noetic-robot-localization ros-noetic-combined-robot-hw ros-noetic-joint-limits-interface ros-noetic-gmapping ros-noetic-amcl ros-noetic-position-controllers ros-noetic-joint-trajectory-controller ros-noetic-moveit-visual-tools ros-noetic-moveit-ros-planning-interface ros-noetic-ros-control ros-noetic-ros-controllers ros-noetic-global-planner ros-noetic-joint-state-publisher-gui ros-noetic-gazebo-ros-control
-        sudo apt-get install ros-noetic-catkin python-catkin-tools
+	sudo apt-get install ros-noetic-control-* ros-noetic-controller-* ros-noetic-effort-controllers ros-noetic-position-controllers	
+        sudo apt-get install ros-noetic-velocity-controllers ros-noetic-twist-mux ros-noetic-diff-drive-controller ros-noetic-costmap-prohibition-layer ros-noetic-moveit ros-noetic-moveit-core ros-noetic-teb-local-planner ros-noetic-move-base ros-noetic-moveit-kinematics ros-noetic-robot-localization ros-noetic-combined-robot-hw ros-noetic-joint-limits-interface ros-noetic-gmapping ros-noetic-amcl ros-noetic-position-controllers ros-noetic-joint-trajectory-controller ros-noetic-moveit-visual-tools ros-noetic-moveit-ros-planning-interface ros-noetic-ros-control ros-noetic-ros-controllers ros-noetic-global-planner ros-noetic-gazebo-ros-control
+        sudo apt-get install ros-noetic-catkin
 fi
 
 # Environment Setup
@@ -56,7 +62,7 @@ echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 # Dependencies for building packages
-sudo apt install python-pip python-rosdep python-rosinstall python-rosinstall-generator python-wstool
+sudo apt install python-pip python-rosdep python-rosinstall python-rosinstall-generator python-wstool python-catkin-tools
 
 # Initialize rosdep
 sudo rosdep init
